4f067e110da71db1d3d9e03d665e4580ef3b63ef
ccortesbustamante1@gmail.com <ccortesbustamante1@gmail.com>
2023-11-23 19:49:50 -0500

    Se realizaron cambios en el inventario, añadiendole al vector checkbox para eliminar cada producto

--- scripts/inventario.js
+++ scripts/inventario.js
@@ -1,47 +1,112 @@
-let inventory = [
-    { code: "2356", name: 'Martillo', quantity: 10 },
-    { code: "0034", name: 'Destornillador', quantity: 20 }
-];
-
-let currentUser = null;
-
-
-function showMessage(message) {
-    document.getElementById('message').textContent = message;
-}
-
-function showInventory() {
-    const productList = document.getElementById('product-list');
-    productList.innerHTML = '';
-
-    inventory.forEach(product => {
-        const li = document.createElement('li');
-        li.textContent = `Código: ${product.code}  Nombre: ${product.name}  Cantidad: ${product.quantity}`;
-        productList.appendChild(li);
-    });
-}
-
-function showAddProductForm() {
-}
-
-function hideAddProductForm() {
-    showMessage('');
-}
-
-function addProduct() {
-    const codeProduct = document.getElementById('product-code').value;
-    const productName = document.getElementById('product-name').value;
-    const productQuantity = parseInt(document.getElementById('product-quantity').value, 10);
-
-    if (productName && !isNaN(productQuantity) && productQuantity > 0 && codeProduct.length == 4) {
-        inventory.push({code: codeProduct, name: productName, quantity: productQuantity });
-        showInventory();
-        hideAddProductForm();
-        showMessage('Producto agregado al inventario.');
-    } else {
-        showMessage('Por favor, ingrese un nombre de producto válido y una cantidad válida.');
+//este es el nuestro
+// Vector para almacenar productos
+let products = [
+    {
+      code: "001",
+      name: "Computador",
+      description: "Potente portátil para trabajo y juegos.",
+      price: 4450000.00
+    },
+    {
+      code: "002",
+      name: "Celular",
+      description: "Último smartphone con características avanzadas.",
+      price: 3230000.00
+    },
+    {
+      code: "003",
+      name: "Audífonos",
+      description: "Audífonos inalámbricos con cancelación de ruido.",
+      price: 230000.00
     }
-}
+  ];
 
-showInventory()
+
+// Función para agregar un producto
+function addProduct() {
+    const productCode = document.getElementById('product-code').value;
+    const productName = document.getElementById('product-name').value;
+    const productQuantity = document.getElementById('product-quantity').value;
+    const productPrice = document.getElementById('product-price').value;
+
+    if (productName === '' || productQuantity === '' || productCode === '' || productPrice === '') {
+        alert('Por favor, complete todos los campos');
+        return;
+      }
+      products.push({
+        code: productCode,
+        name: productName,
+        description: 'Cantidad: ' + productQuantity,
+        price: parseFloat(productPrice)
+    });
+
+      let nuevoProducto = document.createElement('li');
+      nuevoProducto.textContent = 'Código: ' + productCode + ' - ' + productName + ' - Cantidad: ' + productQuantity + productPrice;
+
+      document.getElementById('productList').appendChild(nuevoProducto);
+
+      document.getElementById('product-name').value = '';
+      document.getElementById('product-quantity').value = '';
+      document.getElementById('product-code').value = '';
+      document.getElementById('product-price').value = '';
+}  
+
+   //Funcion para Eliminar producto
+  
+/*
+   function eliminarProducto() {
+    var listaProductos = document.getElementById('productList');
+    var productosSeleccionados = document.querySelectorAll('.productItem input:checked');
+    productosSeleccionados.forEach(function(producto) {
+      listaProductos.removeChild(producto.parentNode);
+    });
+  }
+
+  function eliminarProducto() {
+    let listaProductos = document.getElementById('productList');
+    let ultimaPosicion = listaProductos.children.length - 1;
+
+    if (ultimaPosicion >= 0) {
+      listaProductos.removeChild(listaProductos.children[ultimaPosicion]);
+    } else {
+      alert('No hay productos para eliminar');
+    }
+  }*/
+  let productList = document.getElementById('productList');
+
+products.forEach(product => {
+    let productElement = document.createElement('div');
+    productElement.innerHTML = `
+        <input type="checkbox" id="${product.code}">
+        <label for="${product.code}">${product.name} - ${product.description} - ${product.price}</label>
+    `;
+    productList.appendChild(productElement);
+});
+
+let deleteButton = document.getElementById('deleteButton');
+
+deleteButton.addEventListener('click', () => {
+    products = products.filter(product => {
+        let checkbox = document.getElementById(product.code);
+        return !checkbox.checked;
+    });
+
+    // Actualizar la lista de productos en la página
+    productList.innerHTML = '';
+    products.forEach(product => {
+        let productElement = document.createElement('div');
+        productElement.innerHTML = `
+            <input type="checkbox" id="${product.code}">
+            <label for="${product.code}">${product.name} - ${product.description} - ${product.price}</label>
+        `;
+        productList.appendChild(productElement);
+    });
+});
+
+
+
+
+
+
+
 
